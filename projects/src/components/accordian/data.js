771501e835630fbead7f2506e43bf1c959b92fb7
Muteeb Haider <muteebhdr@gmail.com>
2025-07-04 11:43:42 +0500

    Basic folder structure and dummy data

---
+++ projects/src/components/accordian/data.js
@@ -0,0 +1,27 @@
+const data = [
+  {
+    id: 1,
+    question: "What is React?",
+    answer:
+      "React is a JavaScript library used to build user interfaces, especially for single-page applications.",
+  },
+  {
+    id: 2,
+    question: "What is JSX?",
+    answer:
+      "JSX is a syntax extension for JavaScript that looks similar to HTML and is used with React to describe UI elements.",
+  },
+  {
+    id: 3,
+    question: "What are props in React?",
+    answer:
+      "Props are arguments passed into React components, used to send data from one component to another.",
+  },
+  {
+    id: 4,
+    question: "What is state in React?",
+    answer:
+      "State is a built-in object in React that stores property values that belong to the component and can change over time.",
+  },
+];
+export default data;
